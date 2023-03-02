08e815a9fb8373011b5a378b778b37cb1668575e
Will S <willstott101@gmail.com>
2023-03-02 17:26:43 +0000

    Ignore multi-frame messages without creating new message objects

--- src/zeromq/gstzmqsrc.c
+++ src/zeromq/gstzmqsrc.c
@@ -230,16 +230,14 @@ gst_zmq_src_create (GstPushSrc * psrc, GstBuffer ** outbuf)
 
   gst_buffer_unmap (*outbuf, &map);
 
-  // Ignore later frames in multi-frame messages
-  if (zmq_msg_more(&msg)) {
-    while (1) {
-      zmq_msg_t trailing;
-      zmq_msg_init(&trailing);
-      zmq_msg_recv(&trailing, src->socket, 0);
-      zmq_msg_close(&trailing); 
-      if (!zmq_msg_more(&trailing)) break;
-    }
+  // Ignore later frames in multi-part messages
+  while (zmq_msg_more (&msg)) {
+    zmq_msg_close (&msg); 
+    zmq_msg_init (&msg);
+    zmq_msg_recv (&msg, src->socket, 0);
   }
+
+  zmq_msg_close (&msg);
 
   GST_LOG_OBJECT (src, "delivered a buffer of size %" G_GSIZE_FORMAT " bytes",
       msg_size);
